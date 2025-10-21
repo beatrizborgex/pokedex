8ff6d6c597be80a9e3bf1870da7d7a23228b7d80
beatrizborgex <beatrizoficialtato@hotmail.com>
2025-10-21 10:12:39 -0400

    add Home page

---
+++ src/components/Spinner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center p-6">
+    <div class="animate-spin rounded-full h-12 w-12 border-4 border-solid border-current border-t-transparent"></div>
+  </div>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <div class="home">
+    <SearchBar v-model="searchTerm" />
+    <PokemonList :searchTerm="searchTerm" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import SearchBar from '../components/SearchBar.vue'
+import PokemonList from '../components/PokemonList.vue'
+
+const searchTerm = ref('')
+</script>
+
+<style>
+.home {
+  padding: 2rem;
+}
+</style>
